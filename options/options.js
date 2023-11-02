9363f44b033f89529700cb999f11ed2b78c40104
Crowdbotics <team@crowdbotics.com>
2023-11-02 15:45:15 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lendingcircle-44296",
+  url: "https://lendingcircle-44296.botics.co",
+  api: "https://lendingcircle-44296.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
